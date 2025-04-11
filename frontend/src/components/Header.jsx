dd921b48fafb2b22a2cc9032913025e8acd8069c
NeuroTalos <neurotalos.26@yahoo.com>
2025-04-11 09:16:20 +0800

    Updated header.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,66 @@
-// Header.js
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Menu} from 'antd';
+import { 
+  AppstoreOutlined, 
+  UserAddOutlined,
+  LoginOutlined,
+} from '@ant-design/icons';
+import { Link, useNavigate } from 'react-router-dom';
+
 
 const Header = () => {
+  const menuStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#001529',
+    padding: '0 20px',
+    height: '55px',
+  };
+
+  const navigate = useNavigate();
+
   return (
-    <header style={{ backgroundColor: '#3C4A70', padding: '10px', textAlign: 'center', color: 'white' }}>
-      <h1>Header</h1>
-    </header>
+    <div style={menuStyle}>
+      <Menu mode="horizontal" theme="dark" style={{ flex: 1 }}>
+        <Menu.Item key="home" style={{ cursor: 'pointer' }}>
+          <Link to="/" style={{ color: 'inherit' }}>
+            <AppstoreOutlined style={{ fontSize: 20 }}/>
+          </Link>
+        </Menu.Item>
+
+        <div style={{ width: '40px' }} />
+
+        <Menu.Item key="tasks">
+          <a href="#" 
+            style={{ 
+              textDecoration: 'none' ,
+              fontSize: '20px',
+              fontWeight: '400',
+            }}>
+            Задачи
+          </a>
+        </Menu.Item>
+      </Menu>
+
+      <div>
+        <Button 
+          icon={<UserAddOutlined />} 
+          style={{ marginRight: 20 }}
+          onClick={() => navigate('/registration')}
+        >
+          Зарегистрироваться
+        </Button>
+        <Button 
+        type="primary" 
+        icon={<LoginOutlined />}
+        style={{ marginRight: 15 }}
+        onClick={() => navigate('/auth')}
+        >
+          Войти
+        </Button>
+      </div>
+    </div>
   );
 };
 
--- frontend/src/components/home_form/home.jsx
+++ frontend/src/components/home_form/home.jsx
@@ -6,8 +6,6 @@ const HomeWidget = () => {
     return (
         <div className="w-screen h-screen grid place-content-center">
             <h2>Домашняя страница</h2>
-            <br/>
-            <Link to="/registration">Зарегестрироваться</Link>
         </div>
     );
 };
