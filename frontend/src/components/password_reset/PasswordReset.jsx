13605e5d03f362a5084d4d49e5294792b1789394
NeuroTalos <neurotalos.26@yahoo.com>
2025-06-12 14:06:05 +0800

    Fix axios.post for password_reset again

--- frontend/src/components/password_reset/PasswordReset.jsx
+++ frontend/src/components/password_reset/PasswordReset.jsx
@@ -18,7 +18,7 @@ const PasswordSetPage = () => {
     }
 
     try {
-      await axios.post(`http://${API_URL}/email/update_password`, {
+      await axios.post(`${API_URL}/email/update_password`, {
         token,
         new_password: password,
       });
